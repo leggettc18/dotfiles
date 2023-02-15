20c190ffacc1f579fd04bab3075ce098b5b6ef8b
Christopher Leggett <chris@leggett.dev>
2023-02-15 13:34:45 -0500

    neovim plugin and colorscheme updates

--- .config/nvim/lua/chris/core/colorscheme.lua
+++ .config/nvim/lua/chris/core/colorscheme.lua
@@ -4,3 +4,14 @@ if not status then
     print("Colorscheme not found!")
     return
 end
+
+vim.cmd("highlight Normal ctermbg=none guibg=none")
+vim.cmd("highlight NonText ctermbg=none guibg=none")
+vim.cmd("highlight EndOfBuffer ctermbg=none guibg=none")
+vim.cmd("highlight LineNr ctermbg=none guibg=none")
+vim.cmd("highlight Folded ctermbg=none guibg=none")
+vim.cmd("highlight SpecialKey ctermbg=none guibg=none")
+vim.cmd("highlight VertSplit ctermbg=none guibg=none")
+vim.cmd("highlight SignColumn ctermbg=none guibg=none")
+vim.cmd("highlight EndOfFile ctermbg=none guibg=none")
+vim.cmd("highlight NormalNC ctermbg=none guibg=none")
